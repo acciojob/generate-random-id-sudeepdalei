3c8c955e4df8189f148f9f3e70c551942060833d
Sudeep Kumar Dalei <sudeepdalei38@gmail.com>
2026-05-01 22:02:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
-  // write your code here
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < l; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    result += chars[randomIndex];
+  }
+
+  return result;
 }
 
 // Do not change the code below.
